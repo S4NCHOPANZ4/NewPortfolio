b60645c801c9210d7aad431c67addb8f81918be2
Juan Buitrago <buitr4go@gmail.com>
2023-10-29 00:40:30 -0700

    fixed comp props

--- src/Pages/Desktop.jsx
+++ src/Pages/Desktop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import Navbar from "../Components/Navbar";
 import Icon from "../Components/Icon";
 import MyPortfolioIcon from "../assets/icons/iconPortfolio.png";
@@ -19,12 +19,13 @@ const Desktop = ({ setClicked }) => {
       icon: "https://win98icons.alexmeub.com/icons/png/html-1.png",
       minimized: false,
       selected: true,
-      content: <Portfiolio/>
+      content: Portfiolio,
+
     },
   ]);
 
 
-  const openWindow = (name, icon, content) =>{
+  const openWindow = (name, icon, content, props) =>{
       let newList = [...tabs]
 
 
@@ -57,15 +58,29 @@ const Desktop = ({ setClicked }) => {
             selected: false,
           }
         });
-        newList.push(
-          {
-            name: name,
-            icon: icon,
-            minimized: false,
-            selected: true,
-            content: content
-          }
-        )
+        if(props){
+          newList.push(
+            {
+              name: name,
+              icon: icon,
+              minimized: false,
+              selected: true,
+              content: content,
+              props: props
+            }
+          )
+        }
+        else{
+          newList.push(
+            {
+              name: name,
+              icon: icon,
+              minimized: false,
+              selected: true,
+              content: content
+            }
+          )
+        }
         setTabs(newList)
       }
       return
@@ -80,7 +95,11 @@ const Desktop = ({ setClicked }) => {
       {tabs.map((tab, i) => {
         return (
           <WindowUI tabs={tabs} setTabs={setTabs} tab={tab} i={i} key={i} title={tab.name} icon={tab.icon} minimized={tab.minimized}>
-            {tab.content}
+            {tab.props? 
+            <tab.content {...tab.props}/>
+            :
+            <tab.content/>
+          }
           </WindowUI>
         );
       })}
@@ -99,64 +118,66 @@ const Desktop = ({ setClicked }) => {
       </div>
       {/* MY PORTFOLIO ICON */}
       <div
-      onClick={() => openWindow("My Portfolio", MyPortfolioIcon, <Portfiolio/>)}
+      onClick={() => openWindow("My Portfolio", MyPortfolioIcon, Portfiolio)}
       className="absolute left-4 top-2">
         <Icon name={"My Portfolio"} icon={MyPortfolioIcon} />
       </div>
       {/* CASE JOLTS */}
       <div 
       // icon, image, tools, demo, repo, name, desc
-      onClick={() => openWindow("Case Jolt", CaseJoltIcon, <ProjectOverlay 
-      icon={projects.caseJolt.icon} 
-      image={projects.caseJolt.backgound}
-      tools={projects.caseJolt.icons}
-      demo={projects.caseJolt.demo}
-      repo={projects.caseJolt.url}
-      name={projects.caseJolt.name}
-      desc={projects.caseJolt.desc}
-      />)}
+      onClick={() => openWindow("Case Jolt", CaseJoltIcon,  ProjectOverlay, {
+        icon: projects.caseJolt.icon, 
+        image: projects.caseJolt.backgound, 
+        tools: projects.caseJolt.icons, 
+        demo: projects.caseJolt.demo,
+        repo: projects.caseJolt.url,
+        name: projects.caseJolt.name,
+        desc: projects.caseJolt.desc
+      } )}
       className="absolute left-4 top-20">
         <Icon name={"Case Jolt"} icon={CaseJoltIcon} />
       </div>
       {/* FILM FOLIA */}
       <div
-      onClick={() => openWindow("Film Folia", filmfoliaIcon, <ProjectOverlay 
-      icon={projects.FilmFolia.icon} 
-      image={projects.FilmFolia.backgound}
-      tools={projects.FilmFolia.icons}
-      demo={projects.FilmFolia.demo}
-      repo={projects.FilmFolia.url}
-      name={projects.FilmFolia.name}
-      desc={projects.FilmFolia.desc}
-      />)}
+      onClick={() => openWindow("Film Folia", filmfoliaIcon, ProjectOverlay, {
+        icon: projects.FilmFolia.icon, 
+        image: projects.FilmFolia.backgound, 
+        tools: projects.FilmFolia.icons, 
+        demo: projects.FilmFolia.demo,
+        repo: projects.FilmFolia.url,
+        name: projects.FilmFolia.name,
+        desc: projects.FilmFolia.desc
+      })}
       className="absolute left-4 top-40">
         <Icon name={"Film Folia"} icon={filmfoliaIcon} />
       </div>
       {/* FITHUB */}
       <div 
-      onClick={() => openWindow("FitHub", FitHubIcon, <ProjectOverlay 
-      icon={projects.fitHub.icon} 
-      image={projects.fitHub.backgound}
-      tools={projects.fitHub.icons}
-      demo={projects.fitHub.demo}
-      repo={projects.fitHub.url}
-      name={projects.fitHub.name}
-      desc={projects.fitHub.desc}
-      />)}
+      onClick={() => openWindow("FitHub", FitHubIcon, ProjectOverlay,  {
+        icon: projects.fitHub.icon, 
+        image: projects.fitHub.backgound, 
+        tools: projects.fitHub.icons, 
+        demo: projects.fitHub.demo,
+        repo: projects.fitHub.url,
+        name: projects.fitHub.name,
+        desc: projects.fitHub.desc
+      })}
       className="absolute left-4 top-60">
         <Icon name={"FitHub"} icon={FitHubIcon} />
       </div>
       {/* STEREO */}
       <div 
-      onClick={() => openWindow("Stereo", StereohIcon,  <ProjectOverlay 
-      icon={projects.Stereo.icon} 
-      image={projects.Stereo.backgound}
-      tools={projects.Stereo.icons}
-      demo={projects.Stereo.demo}
-      repo={projects.Stereo.url}
-      name={projects.Stereo.name}
-      desc={projects.Stereo.desc}
-      />)}
+      onClick={() => openWindow("Stereo", StereohIcon,  ProjectOverlay,
+      {
+        icon: projects.Stereo.icon, 
+        image: projects.Stereo.backgound, 
+        tools: projects.Stereo.icons, 
+        demo: projects.Stereo.demo,
+        repo: projects.Stereo.url,
+        name: projects.Stereo.name,
+        desc: projects.Stereo.desc
+      }
+      )}
       className="absolute left-4 top-80">
         <Icon name={"Stereo"} icon={StereohIcon} />
       </div>
