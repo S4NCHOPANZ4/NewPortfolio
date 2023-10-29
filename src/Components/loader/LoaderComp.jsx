53ba1e852b54333c9b72f8933abf01967ceeab78
Juan Buitrago <buitr4go@gmail.com>
2023-10-29 00:25:11 -0700

    full v1

--- src/Components/loader/LoaderComp.jsx
+++ src/Components/loader/LoaderComp.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-const LoaderComp = () => {
+const LoaderComp = ({ loaded }) => {
+  const controlsLeft = useAnimation();
+  const controlsRight = useAnimation();
+
+  useEffect(() => {
+    if (loaded) {
+      abrirTelon();
+    }
+  }, [loaded]);
+
+  const abrirTelon = async () => {
+    // Inicia ambas animaciones al mismo tiempo
+    await Promise.all([
+      controlsLeft.start({ x: "-100vw" }, { duration: 2.5 }), // Duración de 2 segundos
+      controlsRight.start({ x: "100vw" }, { duration: 2.5 }), // Duración de 2 segundos
+    ]);
+  };
   return (
-    <div>LoaderComp</div>
-  )
-}
+    <div className="flex" style={{ height: "100%", overflow: "hidden" }}>
+      {/* Div Izquierdo */}
+      <motion.div
+        style={{
+          flex: 1,
+          backgroundColor: "black",
+          height: "100%",
+          width: "100vw",
+        }}
+        animate={controlsLeft}
+      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: loaded ? 0 : 1 }}
+        transition={{ duration: 0 }}
+        className="fixed flex flex-col justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+      <div className="custom-loader"></div>
+      </motion.div>
+      <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: loaded ? 0 : 1 }}
+          transition={{ duration: 0 }}
+      className="text-[#f1f1f1]  bottom-10 max-w-[250px] text-center fixed left-1/2 transform -translate-x-1/2">
+        <h1 >use desktop and microsoft edge for a better experience</h1>
 
-export default LoaderComp
+      </motion.div>
+      {/* Div Derecho */}
+      <motion.div
+        style={{
+          flex: 1,
+          backgroundColor: "black",
+          height: "100%",
+          width: "100vw",
+        }}
+        animate={controlsRight}
+      />
+    </div>
+  );
+};
+export default LoaderComp;
--- src/Treejs/Envtiroment.jsx
+++ src/Treejs/Envtiroment.jsx
@@ -1,23 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  ContactShadows,
-  Environment,
-  OrbitControls,
-} from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { ComputerModel } from "./Components/ComputerModel";
 import FuzzyOverlay from "../Components/FuzzyOverlay";
+import LoaderComp from "../Components/loader/LoaderComp";
+import { motion } from "framer-motion";
 
 const Envtiroment = () => {
   const cameraRef = useRef();
 
   const [watching, setWatching] = useState(false);
   const [loadedModel, setLoadedModel] = useState(false);
-  const [fov, setFov] = useState(null); //40
+  const [fov, setFov] = useState(null);
 
   useEffect(() => {
-
-
     const handleResize = () => {
       if (window.innerWidth <= 340) {
         setFov(90);
@@ -33,15 +29,59 @@ const Envtiroment = () => {
 
     window.addEventListener("resize", handleResize);
   }, []);
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    // Function to update the time every second
+    const updateClock = () => {
+      setTime(new Date());
+    };
+
+    // Update the time initially
+    updateClock();
+
+    // Set up an interval to update the time every second
+    const interval = setInterval(updateClock, 1000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []); // The empty array ensures the effect runs only once on component mount
+
+  // Format the time in HH:mm:ss format
+  const formattedTime = time.toLocaleTimeString();
+
   return (
     <>
-
-        <div className={`${loadedModel? 'opacity-1' : 'opacity-0'} h-full w-full bg-[#1f1f22f4]`}>
-        <FuzzyOverlay opacity={3}/>
-          <div className="fixed left-40 bottom-20 bg-black px-4 py-2 ">
-            <h1 className="text-white">fe</h1>
-          </div>
-          {fov && (
+      <div className="h-full w-full fixed">
+        <LoaderComp loaded={loadedModel} />
+      </div>
+      <div
+        className={`${
+          loadedModel ? "opacity-1" : "opacity-0"
+        } h-full w-full bg-[#1f1f22f4]`}
+      >
+        <FuzzyOverlay opacity={3} />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: loadedModel ? 1 : 0 }}
+          transition={{ duration: 2 }}
+          className="fixed left-40 top-20  "
+        >
+          <h1 className="text-white bg-black px-4 py-2 w-[350px]">
+            Hi! I'm Juan, Web Developer | {formattedTime}
+          </h1>
+          <h1 className="text-white bg-black px-4 py-2 mt-2 w-[300px] italic text-sm">
+            Click On the keyboard to continue
+          </h1>
+        </motion.div>
+        {fov && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: loadedModel ? 1 : 0 }}
+            transition={{ duration: 2 }} //
+            className="h-full w-full"
+          >
             <Canvas
               className={`canvas  `}
               camera={{ position: [30, 80, -110], fov: fov }}
@@ -71,8 +111,9 @@ const Envtiroment = () => {
                 far={16}
               />
             </Canvas>
-          )}
-        </div>
+          </motion.div>
+        )}
+      </div>
     </>
   );
 };
